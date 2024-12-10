a07a0e60b5a0970447d9148ae02bd9a8fa435c1d
liam-lamming <142473324+liam-lamming@users.noreply.github.com>
2024-12-10 13:56:59 -0700

    Initial commit: Implemented task creation, deletion, and edit features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -45,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation(libs.firebase.analytics)
 }
